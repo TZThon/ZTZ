f414c950748d31d7ed676c9872108c232d689294
ZThon-Ar <129999331+ZThon-Ar@users.noreply.github.com>
2024-05-03 00:56:13 +0300

    Update iytdl.py

--- zelz/assistant/iytdl.py
+++ zelz/assistant/iytdl.py
@@ -18,7 +18,7 @@ from telethon.events import CallbackQuery
 from telethon.utils import get_attributes
 from wget import download
 
-from zira import zedub
+from zelz import zedub
 
 from ..Config import Config
 from ..core import check_owner, pool
@@ -40,7 +40,7 @@ BASE_YT_URL = "https://www.youtube.com/watch?v="
 YOUTUBE_REGEX = re.compile(
     r"(?:youtube\.com|youtu\.be)/(?:[\w-]+\?v=|embed/|v/|shorts/)?([\w-]{11})"
 )
-PATH = "./zira/cache/ytsearch.json"
+PATH = "./zelz/cache/ytsearch.json"
 plugin_category = "البوت"
 
 
