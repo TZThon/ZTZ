96b9411db331266ce044a4ade16bcb51c15bac82
ZThon-Ar <129999331+ZThon-Ar@users.noreply.github.com>
2024-05-03 00:30:32 +0300

    Update converter.py

--- zelz/helpers/functions/converter.py
+++ zelz/helpers/functions/converter.py
@@ -2,11 +2,11 @@ import os
 
 from PIL import Image
 
-from zira.core.logger import logging
-from zira.core.managers import edit_or_reply
-from zira.helpers.functions.vidtools import take_screen_shot
-from zira.helpers.tools import fileinfo, media_type, meme_type
-from zira.helpers.utils.utils import runcmd
+from zelz.core.logger import logging
+from zelz.core.managers import edit_or_reply
+from zelz.helpers.functions.vidtools import take_screen_shot
+from zelz.helpers.tools import fileinfo, media_type, meme_type
+from zelz.helpers.utils.utils import runcmd
 
 LOGS = logging.getLogger(__name__)
 
