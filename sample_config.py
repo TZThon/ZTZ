8a893e2989a997fd4e34b28863f957f0610cf795
Ahmedhhhhhgg <97369627+Ahmedhhhhhgg@users.noreply.github.com>
2024-06-14 17:51:42 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -19,7 +19,7 @@ class Config(object):
     APP_ID = int(os.environ.get("APP_ID", 6))
     API_HASH = os.environ.get("API_HASH") or None
     # Datbase url heroku sets it automatically else get this from elephantsql
-    DB_URI = os.environ.get("DATABASE_URL", None)
+    DB_URI = os.environ.get("SCALINGO_POSTGRESQL_URL", None)
     # Get this value by running python3 stringsetup.py or https://repl.it/@zedthonn/stringsession
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
     # Telegram BOT Token and bot username from @BotFather
