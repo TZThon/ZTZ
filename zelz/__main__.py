656fde3b3bc47845a1b340e383e595e78bb4057e
Ahmedhhhhhgg <97369627+Ahmedhhhhhgg@users.noreply.github.com>
2024-06-14 17:04:55 +0300

    Update __main__.py

--- zelz/__main__.py
+++ zelz/__main__.py
@@ -1,10 +1,8 @@
 import sys, asyncio
-from aiohttp import web
 import zelz
 from zelz import BOTLOG_CHATID, PM_LOGGER_GROUP_ID, tbot
 from .Config import Config
 from .core.logger import logging
-from .core.server import web_server
 from .core.session import zedub
 from .utils import (
     add_bot_to_logger_group,
@@ -19,20 +17,7 @@ LOGS = logging.getLogger("Zelzal")
 cmdhr = Config.COMMAND_HAND_LER
 
 
-async def zthons(session=None, client=None, session_name="Main"):
-    if session:
-        LOGS.info(f"⌭ جار بدء الجلسة [{session_name}] ⌭")
-        try:
-            await client.start()
-            return 1
-        except:
-            LOGS.error(f"خطأ في الجلسة {session_name}!! تأكد وحاول مجددا !")
-            return 0
-    else:
-        return 0
-
-
-async def zthonstart(total):
+async def zthonstart():
     await setup_bot()
     await mybot()
     await verifyLoggerGroup()
@@ -45,21 +30,10 @@ async def zthonstart(total):
 
 async def start_zthon():
     try:
-        tbot_id = await tbot.get_me()
-        Config.TG_BOT_USERNAME = f"@{tbot_id.username}"
-        zedub.tgbot = tbot
-        LOGS.info("⌭ بـدء تنزيـل زدثــون ⌭")
-        CLIENTR = await zthons(Config.STRING_SESSION, zedub, "STRING_SESSION")
-        await tbot.start()
-        total = CLIENTR
         await load_plugins("plugins")
         await load_plugins("assistant")
         LOGS.info(f"⌔ تـم تنصيـب زدثــون . . بنجـاح ✓ \n⌔ لـ إظهـار الاوامـر ارسـل (.الاوامر)")
-        await zthonstart(total)
-        app = web.AppRunner(await web_server())
-        await app.setup()
-        bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, Config.PORT).start()
+        await zthonstart()
     except Exception as e:
         LOGS.error(f"{str(e)}")
         sys.exit()
