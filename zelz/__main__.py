4148dd08033d76fb43a1009c3d67058b2ead1f48
Ahmedhhhhhgg <97369627+Ahmedhhhhhgg@users.noreply.github.com>
2024-06-14 19:51:42 +0300

    Add files via upload

---
+++ zelz/__main__.py
@@ -0,0 +1,69 @@
+import sys, asyncio
+import zelz
+from zelz import BOTLOG_CHATID, HEROKU_APP, PM_LOGGER_GROUP_ID
+from telethon import functions
+from .Config import Config
+from .core.logger import logging
+from .core.session import zedub
+from .utils import mybot, saves
+from .utils import add_bot_to_logger_group, load_plugins, setup_bot, startupmessage, verifyLoggerGroup
+
+LOGS = logging.getLogger("Zelzal")
+cmdhr = Config.COMMAND_HAND_LER
+
+try:
+    LOGS.info("⌭ بـدء تنزيـل زدثــون ⌭")
+    zedub.loop.run_until_complete(setup_bot())
+    LOGS.info("✓ تـم تنزيـل زدثــون .. بـنجـاح ✓")
+except Exception as e:
+    LOGS.error(f"{str(e)}")
+    sys.exit()
+
+class ZTCheck:
+    def __init__(self):
+        self.sucess = True
+ZTcheck = ZTCheck()
+
+try:
+    LOGS.info("⌭ بـدء إنشـاء البـوت التلقـائـي ⌭")
+    zedub.loop.run_until_complete(mybot())
+    LOGS.info("✓ تـم إنشـاء البـوت .. بـنجـاح ✓")
+except Exception as e:
+    LOGS.error(f"- {e}")
+
+try:
+    LOGS.info("⌭ جـارِ تفعيـل الاشتـراك ⌭")
+    zedub.loop.create_task(saves())
+    LOGS.info("✓ تـم تفعيـل الاشتـراك .. بنجـاح ✓")
+except Exception as e:
+    LOGS.error(f"- {e}")
+
+
+async def startup_process():
+    await verifyLoggerGroup()
+    await load_plugins("plugins")
+    await load_plugins("assistant")
+    LOGS.info(f"⌔ تـم تنصيـب زدثــون . . بنجـاح ✓ \n⌔ لـ إظهـار الاوامـر ارسـل (.الاوامر)")
+    await verifyLoggerGroup()
+    await add_bot_to_logger_group(BOTLOG_CHATID)
+    if PM_LOGGER_GROUP_ID != -100:
+        await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
+    await startupmessage()
+    ZTcheck.sucess = True
+    return
+
+
+zedub.loop.run_until_complete(startup_process())
+
+if len(sys.argv) not in (1, 3, 4):
+    zedub.disconnect()
+elif not ZTcheck.sucess:
+    try:
+        zedub.run_until_disconnected()
+    except ConnectionError:
+        pass
+else:
+    try:
+        zedub.run_until_disconnected()
+    except ConnectionError:
+        pass
