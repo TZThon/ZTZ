3572d1868974890a264bde39b4037a9f3fe76d9b
TZThon <168462197+TZThon@users.noreply.github.com>
2024-06-19 00:50:00 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -31,7 +31,7 @@ class Config(object):
     TZ = os.environ.get("TZ", "Asia/Baghdad")
     # set this with required zed repo link
     UPSTREAM_REPO = os.environ.get(
-        "UPSTREAM_REPO", "https://github.com/TheZThon/ZTZ"
+        "UPSTREAM_REPO", "https://github.com/TZThon/ZTZ"
     )
     # External plugins repo
     EXTERNAL_REPO = os.environ.get("EXTERNAL_REPO", None)
